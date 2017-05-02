e7629eb96ef331d88eeaf8886a5ad35a93068fe8
Maryam1362 <maryam.sarab@gmail.com>
2017-05-02 17:42:52 -0400

    few typo fixes

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,6 +1,5 @@
 var app = angular.module('SampleApp',['ngRoute']);
 angular.module('MainCtrl', []).controller('MainController', function($scope) {
 
-	$scope.tagline = 'To the moon and back!';	
 
 });
